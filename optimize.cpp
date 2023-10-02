787452f2f78664d2b5d08633d48e10489b318661
Animuxa <90541509+GreenLuy@users.noreply.github.com>
2023-10-02 19:19:31 +0700

    Update optimize.cpp

--- optimize.cpp
+++ optimize.cpp
@@ -36,9 +36,6 @@ int main()
    D res = procc(n, i);
    system_clock::time_point end = system_clock::now();
    printf_s("Результат: %Lf \n", res);
-   //auto t1 = std::chrono::high_resolution_clock::now();
-   //D res = procc(n, i);
-   //auto t2 = std::chrono::high_resolution_clock::now();
    auto sec = duration_cast<duration<double>>(end - start);
    printf_s("Время выполнения: %lf ms.\n", sec.count()) /*std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count())*/;
 
